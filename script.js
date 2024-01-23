75b14f3967e0143b58f9575a4faa3dd313995b0c
Nirab Shrestha <074msccd011.nirab@pcampus.edu.np>
2024-01-23 17:20:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 var mymap = L.map('mymap').setView([38.60, -90.677], 12);
 
   // load a tile layer
- L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20,
-	ext: 'png'
-}).addTo(mymap);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 var cameraIcon = L.icon({
     iconUrl: 'https://cdn3.iconfinder.com/data/icons/linecons-free-vector-icons-pack/32/camera-128.png', 
